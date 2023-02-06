2bf29390813cd860da5bbc738f2b065552f86998
hilberturi <122933097+hilberturi@users.noreply.github.com>
2023-02-06 14:19:39 +0000

    resolved issue from code review (change of return type)

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ struct T
 
 struct TPtrComparator                                //4
 {
-    const T* compare(const T& a, const T& b) //5
+    T* compare(T& a, T& b) //5
     {
         if( a.value < b.value ) return &a;
         if( a.value > b.value ) return &b;        
